2ea1ddab6b1ede339493a450294c54537c3c72f5
Ihor Osadchyi <igorosa.com@gmail.com>
2021-09-26 12:32:34 +0300

    feat: ahow current user data on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import {
+  Col, Container, Row, Table,
+} from 'react-bootstrap';
+import { useSession } from '../firebase/UserProvider';
 
 const Profile = () => {
-//   const { user } = useSession();
-  let user;
+  const { user } = useSession();
 
   return (
     <Container className="pb-5">
@@ -16,7 +18,22 @@ const Profile = () => {
       </Row>
       <Row className="d-flex justify-content-center">
         <Col className="col-6">
-          User`s data data here :)
+          <Table striped bordered hover variant="dark">
+            <tbody>
+              <tr>
+                <td>id</td>
+                <td>{user.uid}</td>
+              </tr>
+              <tr>
+                <td>Name</td>
+                <td>{user.displayName}</td>
+              </tr>
+              <tr>
+                <td>email</td>
+                <td>{user.email}</td>
+              </tr>
+            </tbody>
+          </Table>
         </Col>
       </Row>
     </Container>
