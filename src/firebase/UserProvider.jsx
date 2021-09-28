49ac40334710a2acc2c6af6ab398289c30226157
Ihor Osadchyi <igorosa.com@gmail.com>
2021-09-28 23:07:41 +0300

    fix: remove redundant code

--- src/firebase/UserProvider.jsx
+++ src/firebase/UserProvider.jsx
@@ -8,19 +8,11 @@ export const UserProvider = ({ children }) => {
   const [session, setSession] = useState({
     user: null,
     loading: true,
-    isAdmin: false,
   });
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
-      let isAdmin = false;
-
-      if (user) {
-        const token = await user.getIdTokenResult();
-        isAdmin = token.claims.admin;
-      }
-
-      setSession({ loading: false, user, isAdmin });
+      setSession({ loading: false, user });
     });
 
     return () => unsubscribe();
